40c4664ec6444e7b1565d869f9bcf29e94f51699
Abdus <abdus@abdus.net>
2024-11-04 00:47:33 +0530

    fix: add footer

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -2,8 +2,12 @@ import Container from "@/app/_components/container";
 
 export function Footer() {
   return (
-    <footer className="bg-neutral-50 border-t border-border bg-background">
-      <Container></Container>
+    <footer className="bg-neutral-50 border-t border-border bg-background flex items-center justify-center gap-3 py-4 w-full">
+        <a href="https://linkedin.com/in/azabdus">LinkedIn</a>
+        &middot;
+        abdus at abdus dot net
+        &middot;
+        <a href="https://github.com/abdus">GitHub</a>
     </footer>
   );
 }
