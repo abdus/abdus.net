4fde7c3051808a0baf4460efcfec4e0a0efdda8c
Abdus <abdus@abdus.net>
2024-11-17 11:35:13 +0530

    fix: tag spacing

--- src/app/_components/post-tags.tsx
+++ src/app/_components/post-tags.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export function PostTags(props: { tags: string[] }) {
   return (
-    <div className="flex flex-wrap gap-4 max-w-2xl mx-auto mt-12 text-red-600">
+    <div className="flex flex-wrap gap-4 mt-12 text-red-600">
       {props.tags.map((tag) => (
         <Link
           key={tag}
