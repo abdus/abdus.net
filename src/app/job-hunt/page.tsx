0bfc2e5b9e3aa0095b17c8e89c5804cc859dff45
Abdus <abdus@abdus.net>
2024-12-23 17:00:41 +0530

    feat: embed resume

--- src/app/job-hunt/page.tsx
+++ src/app/job-hunt/page.tsx
@@ -6,13 +6,11 @@ import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogFooter,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
-import { Textarea } from "@/components/ui/textarea";
 import { Label } from "@radix-ui/react-label";
 import { DownloadIcon } from "lucide-react";
 
@@ -44,92 +42,75 @@ export default function JobHunt() {
             positions that you think I would be a good fit for, please refer me.
           </p>
 
-          <form
-            className="mt-4 grid grid-cols-2 gap-4"
-            onSubmit={(ev) => {
-              ev.preventDefault();
-              alert("Email sent!");
-            }}
-          >
-            <Label>
-              <small className="font-bold">EMAIL</small>
-              <Input
-                type="text"
-                placeholder="person@org.com, manager@org.com"
-              />
-            </Label>
+          <div className="max-w-[80ch] p-4 md:p-8 border border-dashed bg-accent rounded-lg">
+            <div>Dear Hiring Manager,</div>
 
-            <Label>
-              <small className="font-bold">SUBJECT</small>
-              <Input
-                type="text"
-                placeholder="person@org.com"
-                defaultValue="Software Engineer"
-              />
-            </Label>
+            <p>
+              I am looking for a new opportunity and I think I would be a great
+              fit for your team. I have extensive experience with JavaScript and
+              its frameworks, proficiency in AWS, and comfortable in startup
+              environments. I am skilled at designing systems, driven by
+              curiosity, and excel at solving engineering challenges. I thrive
+              in roles that let me stay curious and ship quickly.
+            </p>
+          </div>
 
-            <Label className="col-span-2">
-              <small className="font-bold">
-                EMAIL BODY (you can edit it before sending)
-              </small>
-              <div>
-                Hi, <br /> I am looking for a new opportunity and I think I
-                would be a great fit for your team. I have extensive experience
-                with JavaScript and its frameworks, proficiency in AWS, and
-                comfortable in startup environments. I am skilled at designing
-                systems, driven by curiosity, and excel at solving engineering
-                challenges. I thrive in roles that let me stay curious and ship
-                quickly.
-              </div>
-            </Label>
+          <div className="col-span-2 flex items-center justify-end gap-4 max-w-[80ch] mt-8">
+            <Button
+              type="button"
+              variant="outline"
+              className="flex items-center gap-2"
+            >
+              <DownloadIcon className="size-4" /> Download Resume
+            </Button>
 
-            <div className="col-span-2 flex items-center justify-end gap-4">
-              <Button
-                type="button"
-                variant="outline"
-                className="flex items-center gap-2"
-              >
-                <DownloadIcon className="size-4" /> Download Resume
-              </Button>
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button
+                  type="button"
+                  variant="default"
+                  className="flex items-center gap-2"
+                >
+                  Open Email and Send
+                </Button>
+              </DialogTrigger>
 
-              <Dialog>
-                <DialogTrigger asChild>
-                  <Button
-                    type="button"
-                    variant="default"
-                    className="flex items-center gap-2"
-                  >
-                    Open Email and Send
+              <DialogContent onOpenAutoFocus={(ev) => ev.preventDefault()}>
+                <DialogTitle>Confirm Resume Download</DialogTitle>
+                <p>
+                  As I cannot attach a PDF to an Email directly from this
+                  website, would you like to download the Resume too, and attach
+                  it to the email manually?
+                </p>
+
+                <DialogFooter className="flex gap-2">
+                  <Button type="button" variant="outline">
+                    Open Email
                   </Button>
-                </DialogTrigger>
-
-                <DialogContent onOpenAutoFocus={(ev) => ev.preventDefault()}>
-                  <DialogTitle>Confirm Resume Download</DialogTitle>
-                  <p>
-                    As I cannot attach a PDF to an Email directly from this
-                    website, would you like to download the Resume too, and
-                    attach it to the email manually?
-                  </p>
-
-                  <DialogFooter className="flex gap-2">
-                    <Button type="button" variant="outline">
-                      Open Email
-                    </Button>
-                    <Button type="button">
-                      Download Resume and Open Email
-                    </Button>
-                  </DialogFooter>
-                </DialogContent>
-              </Dialog>
-            </div>
-          </form>
-
+                  <Button type="button">Download Resume and Open Email</Button>
+                </DialogFooter>
+              </DialogContent>
+            </Dialog>
+          </div>
           <h3 className="mt-16">My Resume</h3>
-
           <p>
             I would love some help with my job hunt. If you know of any open
             positions that you think I would be a good fit for, please refer me.
           </p>
+
+          <object
+            type="application/pdf"
+            data="/resume.pdf"
+            className="w-full aspect-[1/1.4]"
+          >
+            <p>
+              This browser does not support PDFs. Please download the PDF to
+              view it:{" "}
+              <a href="/resume.pdf" download="resume_abdus-azad_fullstack.pdf">
+                Download Resume
+              </a>
+            </p>
+          </object>
         </div>
       </Container>
     </div>
